9e63de2ca2811a3e906a91e99d32b522e26bb7ec
lugalnt <luisadriancampossalas10@gmail.com>
2025-02-02 15:51:47 -0700

    Update SOLICITUDprestacionesfinancieras.php

--- SOLICITUDprestacionesfinancieras.php
+++ SOLICITUDprestacionesfinancieras.php
@@ -31,10 +31,28 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"]=="POST")
 {
 
-    $nombre_familiar = $_POST['nombre_familiar'];
+    $nombre_familiar = "%".$_POST['nombre_familiar']."%";
     $tipo = $_POST['tipo'];
     
-   
+   $queryChecarPF = $conn->prepare("SELECT * FROM familiar_empleado f INNER JOIN empleado_familiar e ON f.Id_Familiar = e.Id_Familiar WHERE f.Nombre_Familiar like ? AND e.Id_Empleado = ?");
+    $queryChecarPF->bind_param("si", $nombre_familiar, $_SESSION['Id_Empleado']);
+    $queryChecarPF->execute();
+    $result = $queryChecarPF->get_result();
+    $row = $result->fetch_assoc();
+    $queryChecarPF->close();
+    
+    if ($row)
+    {
+        $queryInsertPF = $conn->prepare("INSERT INTO prestaciones_financieras (Id_Familiar, Tipo_Prestacion) VALUES (?, ?)");
+        $queryInsertPF->bind_param("is", $row['Id_Familiar'], $tipo);
+        $queryInsertPF->execute();
+        $queryInsertPF->close();
+        echo "Solicitud enviada correctamente";
+    }
+    else
+    {
+        echo "Error, no se encontró el familiar";
+    }
 
    
 
