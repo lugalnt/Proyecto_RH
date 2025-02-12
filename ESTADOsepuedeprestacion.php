1e8c0ecfcf21326af54974666ae1f14302026061
lugalnt <luisadriancampossalas10@gmail.com>
2025-02-12 13:39:19 -0700

    k

---
+++ ESTADOsepuedeprestacion.php
@@ -0,0 +1,9 @@
+<?php
+
+//Si en los ultimos 4 meses ya se hizo una solicitud de prestacion, no se puede hacer otra, detener la operacion
+//mandando un bool falso que detenga o no se w jaja muack
+
+require_once("conn.php");
+
+
+?>
