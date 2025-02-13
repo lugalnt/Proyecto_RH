6412ba0563aee50c5f2bfba06dfce7a124401e5c
lugalnt <luisadriancampossalas10@gmail.com>
2025-02-12 19:09:24 -0700

    Update ESTADOsepuedeprestacion.php

--- ESTADOsepuedeprestacion.php
+++ ESTADOsepuedeprestacion.php
@@ -5,5 +5,22 @@
 
 require_once("conn.php");
 
+$fecha_actual = date('Y-m-d');
+$fecha_limite = date('Y-m-d', strtotime('-4 months', strtotime($fecha_actual)));
+
+$queryGAPE = $conn->prepare("SELECT * FROM empleado_prestacion WHERE Numero_Empleado = ? AND Fecha_Otorgamiento BETWEEN ? AND ?");
+$queryGAPE->bind_param("iss", $numeroEmpleado, $fecha_limite, $fecha_actual);
+$queryGAPE->execute();
+$resultGAPE = $queryGAPE->get_result();
+
+while ($rowGAPE = $resultGAPE->fetch_assoc()) {
+    $rowGAPE = $resultGAPE->fetch_assoc();
+    
+        
+
+
+
+}
+
 
 ?>
